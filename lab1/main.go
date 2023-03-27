e0cfb08af1a58bb71497657646f1a370444ecafd
lluckydog <zhanghaowen@mail.ustc.edu.cn>
2023-03-27 09:43:10 +0800

    add lab1

---
+++ lab1/main.go
@@ -0,0 +1,9 @@
+package main
+
+//var two = new(big.Int).SetInt64(2)
+//
+//func main() {
+//	n := new(big.Int).SetInt64(19)
+//	s := new(big.Int).SetInt64(7)
+//	fmt.Printf("res %v \n", Pow(s, new(big.Int).Sub(n, two), n))
+//}
