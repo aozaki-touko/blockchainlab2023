c420ebc171400f3244a1523c374e137e068d9471
lvkaisheng <770890657@qq.com>
2023-06-02 16:12:20 +0800

    Changes

--- lab3/proofofwork.go
+++ lab3/proofofwork.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha256"
 	"math"
 	"math/big"
@@ -18,6 +19,15 @@ type ProofOfWork struct {
 	target *big.Int
 }
 
+type assistStruct struct {
+	Version       int64
+	PrevBlockHash [32]byte
+	MerkleRoot    [32]byte
+	Timestamp     int64
+	Bits          int64
+	Nonce         int64
+}
+
 // NewProofOfWork builds and returns a ProofOfWork
 func NewProofOfWork(b *Block) *ProofOfWork {
 	target := big.NewInt(1)
@@ -28,20 +38,42 @@ func NewProofOfWork(b *Block) *ProofOfWork {
 	return pow
 }
 
+// serilizehead
+func (pow *ProofOfWork) makeHeader(nonce int64) []byte {
+	prevHash := pow.block.GetPrevhash()
+	mkHash := pow.block.Header.MerkleRoot
+	temp := bytes.Join([][]byte{IntToHex(pow.block.Header.Version), prevHash[:], mkHash[:], IntToHex(pow.block.Header.Timestamp), IntToHex(pow.block.Header.Bits), IntToHex(nonce)}, []byte{})
+	return temp
+}
+
 // Run performs a proof-of-work
 // implement
 func (pow *ProofOfWork) Run() (int64, []byte) {
 	nonce := int64(0)
-
-	return nonce, nil
+	for nonce < int64(maxNonce) {
+		headerData := pow.makeHeader(nonce)
+		hashHeader := sha256.Sum256(headerData)
+		hashedVal := new(big.Int).SetBytes(hashHeader[:])
+		if hashedVal.Cmp(pow.target) == -1 {
+			pow.block.Header.Nonce = nonce
+			break
+		} else {
+			nonce++
+		}
+	}
+	return nonce, pow.makeHeader(nonce)
 }
 
 // Validate validates block's PoW
 // implement
 func (pow *ProofOfWork) Validate() bool {
-	ser := pow.block.Serialize()
-	//targetVal := new(big.Int).Exp(big.NewInt(int64(2)), big.NewInt(int64(256)-pow.target.Int64()), nil)
-	hashedSer := sha256.Sum256(ser)
+	prevHash := pow.block.GetPrevhash()
+	mkHash := pow.block.Header.MerkleRoot
+	temp := bytes.Join([][]byte{IntToHex(pow.block.Header.Version), prevHash[:], mkHash[:], IntToHex(pow.block.Header.Timestamp), IntToHex(pow.block.Header.Bits), IntToHex(pow.block.Header.Nonce)}, []byte{})
+
+	//prevHash := pow.block.GetPrevhash()
+	//res = append(res, prevHash[:]...)
+	hashedSer := sha256.Sum256(temp)
 	hashedVal := new(big.Int).SetBytes(hashedSer[:])
 	if pow.target.Cmp(hashedVal) == 1 {
 		return true
